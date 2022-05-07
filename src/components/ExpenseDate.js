af44392bd89700b9dd4f7ca750a897e379a44e4d
Lisia Grocott <lisiagrocott@gmail.com>
2022-05-07 19:14:14 +1200

    create 2 components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const day = props.expense.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.expense.date.toLocaleString("en-US", { month: "long" });
+  const year = props.expense.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
